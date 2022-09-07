01f0b6ca9eacc9d697902704f149861952958c96
Hankus Pankus <henryjacobscode@gmail.com>
2022-09-07 08:08:51 -0400

    finished ch3 and began ch4

---
+++ ch3/sizeof-operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+
+int main() {
+
+	puts("This program demonstrates the functionality of the sizeof operator in C and potentially C++ (since I don't know anything about C++, I can't say for sure)") ; 
+
+	
+	size_t integer = sizeof(int) ; 
+	size_t doble = sizeof(double) ; 
+	size_t lawng = sizeof(long) ; 
+
+
+
+	printf("Integer: %lu, Double: %lu, Lawng: %lu\n", integer, doble, lawng) ; 
+
+	return 0 ; 
+
+
+
+} 
