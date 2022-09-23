723df325862bce096109fd2286c62ff927262d74
Hankus Pankus <henryjacobscode@gmail.com>
2022-09-22 23:42:17 -0400

    Thursday night commit

---
+++ ch8/del.c
@@ -0,0 +1,23 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/stat.h> 
+#include <dirent.h> 
+#include <string.h> 
+#include <stdlib.h> 
+
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+    FILE *file1 = fopen("the_text.txt", "r") ; 
+    
+    struct stat statbuf ; 
+    
+
+
+    return 0 ; 
+}
