b3a3ca5c1535604626a7f3ac709f66ae135ba2ab
Hankus Pankus <henryjacobscode@gmail.com>
2023-07-20 10:39:37 -0400

    Move Computer-Systems-A-Programmers-Approach to PL Repo

--- Computer-Systems-A-Programmers-Perspective-Bryant-OHallaron/ch8/example-programs/system-level-function-call.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int main () {
-    // 1 <-- stdout 
-    // "Hello world\n" <-- Sequence of bytes to 'write' 
-    // 13 <-- Number of bytes to write
-    write(1, "Hello world\n", 13) ; 
-    _exit(0) ; 
-}
